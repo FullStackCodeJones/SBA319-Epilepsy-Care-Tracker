799147c56726479be6ef68cefa32ef796494a348
FullStackCodeJones <fullstackcodejones@gmail.com>
2024-12-08 14:40:03 -0500

    Completed all of initial requirements

--- routes/patientRoutes.js
+++ routes/patientRoutes.js
@@ -22,6 +22,7 @@ router.get("/:id", async (req, res) => {
     }
     res.status(200).json(patient);
   } catch (error) {
+    console.error("Error In GET /:id route:", error.message);
     res.status(500).json({ error: error.message });
   }
 });
@@ -76,6 +77,51 @@ router.get("/:id", async (req, res) => {
     res.json(patient);
   } catch (error) {
     res.status(500).json({ message: "Server Error" });
+  }
+});
+
+// Route to Get Patients Older than A Specific Age
+router.get("/age/above/:age", async (req, res) => {
+  try {
+    const patients = await Patient.find({ age: { $gt: req.params.age } });
+    if (patients.length === 0) {
+      return res
+        .status(404)
+        .json({ error: "NO Patients Found Above This Age" });
+    }
+    res.status(200).json(patients);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+//Route to Get Patients by Medication
+// Get Patients by Medication
+router.get("/medication/:medication", async (req, res) => {
+  try {
+    const patients = await Patient.find({ medication: req.params.medication });
+    if (patients.length === 0) {
+      return res
+        .status(404)
+        .json({ error: "No patients found taking this medication" });
+    }
+    res.status(200).json(patients);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.get("/condition/:condition", async (req, res) => {
+  try {
+    const patients = await Patient.find({ condition: req.params.condition });
+    if (patients.length === 0) {
+      return res
+        .status(400)
+        .json({ error: "No Patients Found with This Condition" });
+    }
+    res.status(200).json(patients);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
 });
 
